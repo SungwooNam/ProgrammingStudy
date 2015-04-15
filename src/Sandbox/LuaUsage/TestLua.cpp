9de2312e4288ff96dd0dff2f0816b3b6b37019ed
Sungwoo <the.sungwoo@gmail.com>
2015-04-15 14:18:00 +0900

    Add LuaUsage test on Sandbox

---
+++ src/Sandbox/LuaUsage/TestLua.cpp
@@ -0,0 +1,16 @@
+#include <stdexcept>
+#include <boost\test\unit_test.hpp>
+#include <boost\shared_ptr.hpp>
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE( TestLua )
+
+BOOST_AUTO_TEST_CASE( ShouldGo ) 
+{  
+	int i = 0;
+	i++;
+
+}
+
+BOOST_AUTO_TEST_SUITE_END() 
