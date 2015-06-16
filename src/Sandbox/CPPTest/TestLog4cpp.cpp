250a22b414c7c4651b3d76759dad338335bc9138
SungwooNam <the.sungwoo@gmail.com>
2015-06-16 23:51:52 +0900

    Update TestLogProperties as used in equipment control

--- src/Sandbox/CPPTest/TestLog4cpp.cpp
+++ src/Sandbox/CPPTest/TestLog4cpp.cpp
@@ -2,40 +2,17 @@
 
 #include <stdexcept>
 #include <boost\test\unit_test.hpp>
-#include <log4cpp/Portability.hh>
 #include <log4cpp/Category.hh>
-#include <log4cpp/Appender.hh>
-#include <log4cpp/OstreamAppender.hh>
-#include <log4cpp/FileAppender.hh>
-#include <log4cpp/Layout.hh>
-#include <log4cpp/BasicLayout.hh>
-#include <log4cpp/Priority.hh>
-#include <log4cpp/NDC.hh>
-#include <log4cpp/PatternLayout.hh>
 #include <log4cpp/PropertyConfigurator.hh>
-
-double calcPi()
-{
-    double denominator = 3.0;
-    double retVal = 4.0;
-    long i;
-    for (i = 0; i < 50000000l; i++)
-    {
-        retVal = retVal - (4.0 / denominator);
-        denominator += 2.0;
-        retVal = retVal + (4.0 /denominator);
-        denominator += 2.0;
-    }
-    return retVal;
-}
 
 
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE( TestLog4cpp )
 
-BOOST_AUTO_TEST_CASE( TestSimple ) 
-{  
+
+BOOST_AUTO_TEST_CASE( TestLogProperties ) 
+{   
     try {
         
 		log4cpp::PropertyConfigurator::configure("testConfig.log4cpp.properties");
@@ -45,44 +22,30 @@ BOOST_AUTO_TEST_CASE( TestSimple )
 		return;
     }
 
-    log4cpp::Category& root = log4cpp::Category::getRoot();
+	log4cpp::Category& root = log4cpp::Category::getRoot();
+	root.warn("Initialization goes weird but you can see this only in the console");
+	 
+    log4cpp::Category& ALL = 
+        log4cpp::Category::getInstance(std::string("ALL"));
+	 
+    ALL.error( "During startup, serial port %s with baud rate %d can't be init!", "COM2", 9600 );
+    ALL.warn( "During intial process, IO Device %d shows io %d get %d", 3 ,4, 5);
 
-    log4cpp::Category& sub1 = 
-        log4cpp::Category::getInstance(std::string("sub1"));
+    log4cpp::Category& SEQUENCE = 
+        log4cpp::Category::getInstance(std::string("ALL.SEQUENCE"));
+	 
+	SEQUENCE.info( "CLAMP start" );
+	SEQUENCE.warn( "CLAMP io sensor %d is %d but should be %d", 24, 1, 0 );
+	SEQUENCE.error( "CLAMP failed as timeout %d elapsed", 100 );
+	 
+    log4cpp::Category& INSPECTION = 
+        log4cpp::Category::getInstance(std::string("ALL.INSPECTION"));
 
-    log4cpp::Category& sub2 = 
-        log4cpp::Category::getInstance(std::string("sub1.sub2"));
-
-    root.error("root error");
-    root.warn("root warn");
-    sub1.error("sub1 error");
-    sub1.warn("sub1 warn");
-
-    calcPi();
-
-    sub2.error("sub2 error");
-    sub2.warn("sub2 warn");
-
-    root.error("root error");
-    root.warn("root warn");
-    sub1.error("sub1 error");
-    sub1.warn("sub1 warn");
-
-#ifdef WIN32
-    Sleep(100);
-#else
-    sleep(3);
-#endif
-
-    sub2.error("sub2 error");
-    sub2.warn("sub2 warn");
-    sub2.error("%s %s %d", "test", "vform", 123);
-    sub2.warnStream() << "streamed warn";
-
-    sub2 << log4cpp::Priority::WARN << "warn2.." << "..warn3..value=" << 0 
-         << log4cpp::eol << "..warn4";
-
-    log4cpp::Category::shutdown();
+	INSPECTION.info( "Inspection start" );
+	INSPECTION.warn( _T("검사 시작시 네트웍 상태 불안정") );
+	INSPECTION.error( "Inspection failed", 100 );
+ 	
+	log4cpp::Category::shutdown();
 }
 
 
